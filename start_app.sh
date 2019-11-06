148b91d975ee20ebd7e4ba35c09a16cc92e24101
François KREMER <krmr.francois@gmail.com>
2019-11-06 19:34:29 +0100

    [DOCKER] Ajout d'un fichier qui build le tout.

---
+++ start_app.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker-compose exec app composer install
+Docker-compose exec app npm install --no-bin-links
+docker-compose exec app cp .env.example .env
+docker-compose exec app php artisan key:generate
+docker-compose exec app php artisan optimize
+docker-compose exec app php artisan migrate --seed
+docker-compose exec app php artisan make:controller MyController
