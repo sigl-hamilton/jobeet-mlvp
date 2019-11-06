7e68cb98a57f3f566965b724a3954c523d569e45
François KREMER <krmr.francois@gmail.com>
2019-11-06 21:05:22 +0100

    [DOCKER] Modification du Start-app.sh.

--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker-compose exec app composer install
-Docker-compose exec app npm install --no-bin-links
+docker-compose exec app npm install --no-bin-links
 docker-compose exec app cp .env.example .env
 docker-compose exec app php artisan key:generate
 docker-compose exec app php artisan optimize
