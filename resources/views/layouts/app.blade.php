99bc38cda8194e8ea4f59cc9099cf7799325bd5d
Dylan ZHAO <dylan.zhao@epita.fr>
2020-01-07 18:03:41 +0100

    Adding our beautiful logo.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Jobeet') }}
+                    <img src="{{ asset('img/logo_jobeet.png') }}" alt="Jobeet" style="max-width: 25%">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
-mix.js('resources/js/register.js', 'public/js');
+mix.js(['resources/js/app.js', 'resources/js/register.js'], 'public/js')
+    .sass('resources/sass/app.scss', 'public/css')
+    .copyDirectory('resources/img', 'public/img');
